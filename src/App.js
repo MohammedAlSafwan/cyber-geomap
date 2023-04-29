a5b3cf9ca7b0d5fd978709efdff409b25efdd24d
Mohammed Al-Safwan <alsafwan.mohammed+github@gmail.com>
2023-04-28 20:48:51 -0400

    before nmap webpackage breaking changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactECharts from "echarts-for-react";
+import nmap from "node-nmap";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [inputHistory, setInputHistory] = useState([]);
+  const [nmapData, setNmapData] = useState(null);
+  const [geoData, setGeoData] = useState([]);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    if (inputValue) {
+      setInputHistory([...inputHistory, inputValue]);
+      const res = await axios.get(`https://ipapi.co/${inputValue}/json/`);
+      const location = {
+        name: res.data.ip,
+        value: [res.data.longitude, res.data.latitude],
+      };
+      setGeoData([...geoData, location]);
+      nmap.nmapLocation = 'nmap'; //default
+      let quickscan = new nmap.QuickScan(`${inputValue}`);
+
+      quickscan.on("complete", (data) => {
+        setNmapData(data);
+      });
+      quickscan.startScan();
+      // const nmap = new Nmap.NmapScan(inputValue);
+      // nmap.on("complete", (data) => {
+      //   setNmapData(data);
+      // });
+      // nmap.startScan();
+    }
+  };
+
+  const getOption = () => {
+    return {
+      backgroundColor: "transparent",
+      geo: {
+        map: "world",
+        roam: true,
+        label: {
+          emphasis: {
+            show: false,
+          },
+        },
+        itemStyle: {
+          normal: {
+            areaColor: "#323c48",
+            borderColor: "#111",
+          },
+          emphasis: {
+            areaColor: "#2a333d",
+          },
+        },
+      },
+      series: [
+        {
+          name: "IP Locations",
+          type: "scatter",
+          coordinateSystem: "geo",
+          data: geoData,
+          symbolSize: 12,
+          label: {
+            normal: {
+              show: false,
+            },
+            emphasis: {
+              show: false,
+            },
+          },
+          itemStyle: {
+            emphasis: {
+              borderColor: "#fff",
+              borderWidth: 1,
+            },
+          },
+        },
+      ],
+    };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Enter IP address or domain name"
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      {nmapData && (
+        <div className="side-panel">
+          {/* Display nmapData as a tree here */}
+        </div>
+      )}
+      <ReactECharts option={getOption()} style={{ height: "100vh" }} />
     </div>
   );
-}
+};
 
 export default App;
